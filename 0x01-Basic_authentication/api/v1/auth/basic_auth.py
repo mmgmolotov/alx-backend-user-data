2952d19222945fee524bfb2d2d791338215b7329
mmgmolotov <mmgmolotov@gmail.com>
2024-05-21 23:39:29 +0100

    task 7 / base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,9 +3,25 @@
 Basic Auth
 """
 from api.v1.auth.auth import Auth
-
+from base64 import b64decode
+from models.user import User
+from typing import TypeVar
 
 class BasicAuth(Auth):
     """
+    Basic Authentication
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+
+        """
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header[6:]
