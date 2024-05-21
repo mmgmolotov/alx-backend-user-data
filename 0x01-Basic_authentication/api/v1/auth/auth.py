a7ae9ea0e82fe0e2acc332fd45488344ec7b11b6
mmgmolotov <mmgmolotov@gmail.com>
2024-05-21 22:33:05 +0100

    task 3 / auth class fix

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,16 +5,16 @@ Auth
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     """
     Authentication
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        
+
         """
         return False
-
 
     def authorization_header(self, request=None) -> str:
         """
@@ -22,16 +22,15 @@ class Auth:
         """
         return
 
-
     def current_user(self, request=None) -> TypeVar('User'):
         """
 
         """
         return
 
+
 if __name__ == "__main__":
     a = Auth()
-
     print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
     print(a.authorization_header())
     print(a.current_user())
