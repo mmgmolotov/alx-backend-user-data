36ac9133d8bac33bb1ccf3620788bcd40c776074
mmgmolotov <mmgmolotov@gmail.com>
2024-05-21 23:16:24 +0100

    task 5 / request validation fix

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -23,6 +23,7 @@ if auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+
 @app.before_request
 def before_request():
     """
@@ -31,7 +32,9 @@ def before_request():
     if auth is None:
         return
 
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
     if not auth.require_auth(request.path, excluded_paths):
         return
 
@@ -40,6 +43,7 @@ def before_request():
 
     if auth.current_user(request) is None:
         return jsonify({"error": "Forbidden"}), 403
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
