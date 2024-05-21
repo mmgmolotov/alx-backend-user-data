b0ea41cc85cbc74e8dc485fdf9f6e824137c2af3
mmgmolotov <mmgmolotov@gmail.com>
2024-05-21 23:57:42 +0100

    task 9 / U C  fixED

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,7 +5,7 @@ Basic Auth
 from api.v1.auth.auth import Auth
 from base64 import b64decode
 from models.user import User
-from typing import TypeVar
+from typing import TypeVar, Tuple
 
 
 class BasicAuth(Auth):
@@ -42,3 +42,18 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+        self, decoded_base64_authorization_header: str
+    ) -> Tuple[str, str]:
+        if decoded_base64_authorization_header is None:
+            return None, None
+
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
