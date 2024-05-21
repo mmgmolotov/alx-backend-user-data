b5636dfb156a8bb26291c5e20b4a1e41f9661643
mmgmolotov <mmgmolotov@gmail.com>
2024-05-21 22:51:41 +0100

    task 4 / updates

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,7 +3,8 @@
 Auth
 """
 from flask import request
-from typing import List, TypeVar
+from typing import List
+from typing import TypeVar
 
 
 class Auth:
@@ -22,13 +23,14 @@ class Auth:
             return True
         if path in excluded_paths:
             return False
-    def authorization_header(self, request=None) -> str:
+
+    def authorization_header(self, request=None) -> None:
         """
 
         """
         return
 
-    def current_user(self, request=None) -> TypeVar('User'):
+    def current_user(self, request=None) -> None:
         """
 
         """
