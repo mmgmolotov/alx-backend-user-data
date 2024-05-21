98428aa5338419c1803e605a9f34826d3f61356d
mmgmolotov <mmgmolotov@gmail.com>
2024-05-21 23:00:23 +0100

    task 4 / updates

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -20,14 +20,21 @@ class Auth:
         Returns True if excluded_paths is None or empty.
         Returns False if path is in excluded_paths.
         """
-        if path and not path.endswith('/'):
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if path.endswith('/') is False:
             path = path + '/'
-        if not path or path not in excluded_paths:
-            return True
-        if not excluded_paths or excluded_paths == []:
-            return True
-        if path in excluded_paths:
-            return False
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('/') is False:
+                excluded_path = excluded_path + '/'
+            if path == excluded_path:
+                return False
+
         return True
 
     def authorization_header(self, request=None) -> None:
