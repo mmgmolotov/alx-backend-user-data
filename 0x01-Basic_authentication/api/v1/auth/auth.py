4106e4475241e200c6523083e4cbdd2d3180b434
mmgmolotov <mmgmolotov@gmail.com>
2024-05-21 22:58:34 +0100

    task 4 / updates

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,7 +13,12 @@ class Auth:
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
+        Determines if the given path requires authentication.
 
+        Returns True if the path is not in the list of excluded_paths.
+        Returns True if path is None.
+        Returns True if excluded_paths is None or empty.
+        Returns False if path is in excluded_paths.
         """
         if path and not path.endswith('/'):
             path = path + '/'
@@ -23,7 +28,7 @@ class Auth:
             return True
         if path in excluded_paths:
             return False
-        return False
+        return True
 
     def authorization_header(self, request=None) -> None:
         """
