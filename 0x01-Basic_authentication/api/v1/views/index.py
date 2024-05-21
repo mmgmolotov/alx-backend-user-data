202db21c41c7762d5f849444a6d24c1bbd5b5a56
mmgmolotov <mmgmolotov@gmail.com>
2024-05-21 21:52:21 +0100

    task 1 / unauthorized fix

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,9 +25,10 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """
-    
+
     """
     return abort(401)
