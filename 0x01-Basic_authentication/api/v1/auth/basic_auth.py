b21b3c38771edc10d4558aa83b756e38f3575e24
mmgmolotov <mmgmolotov@gmail.com>
2024-05-21 23:41:03 +0100

    task 7 / base64 fix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,10 +7,12 @@ from base64 import b64decode
 from models.user import User
 from typing import TypeVar
 
+
 class BasicAuth(Auth):
     """
     Basic Authentication
     """
+
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """
 
