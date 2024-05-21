48c7c5c6694a4fe17c5bc842fe2329abfe0a8e38
mmgmolotov <mmgmolotov@gmail.com>
2024-05-21 22:54:21 +0100

    task 4 / updates

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -23,6 +23,7 @@ class Auth:
             return True
         if path in excluded_paths:
             return False
+        return False
 
     def authorization_header(self, request=None) -> None:
         """
