14b658ccec3dd8a860ac9d093b52629a6380a8a1
mmgmolotov <mmgmolotov@gmail.com>
2024-05-22 00:00:50 +0100

    task 10 / U object  fixED

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,7 +5,7 @@ Basic Auth
 from api.v1.auth.auth import Auth
 from base64 import b64decode
 from models.user import User
-from typing import TypeVar, Tuple
+from typing import TypeVar, Tuple, Optional
 
 
 class BasicAuth(Auth):
@@ -57,3 +57,24 @@ class BasicAuth(Auth):
 
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
+
+    def user_object_from_credentials(
+        self, user_email: str, user_pwd: str
+        ) -> Optional[TypeVar('User')]:
+        """
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        user_list = User.search({'email': user_email})
+        if not user_list:
+            return None
+
+        user = user_list[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
