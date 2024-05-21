ae0983fc957377aab7fc5cddc2d357a1d495deb0
mmgmolotov <mmgmolotov@gmail.com>
2024-05-21 23:28:42 +0100

    task 6 / basic authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,13 +16,14 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth_type = getenv('AUTH_TYPE')
 
-auth = None
-auth_type = getenv('AUTH_TYPE')
 
 if auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+if auth_type == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 @app.before_request
 def before_request():
