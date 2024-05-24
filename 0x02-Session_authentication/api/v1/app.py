c2078af6faf7ba989bdba85d3560b3564483288b
mmgmolotov <mmgmolotov@gmail.com>
2024-05-24 01:43:44 +0100

    task 4 : MORE FIXES

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -48,7 +48,8 @@ def before_request():
         return
 
     if auth.authorization_header(request
-                                 ) is None and auth.session_cookie(request) is None:
+                                 ) is None and auth.session_cookie(
+                                     request) is None:
         abort(401)
 
     request.current_user = auth.current_user(request)
