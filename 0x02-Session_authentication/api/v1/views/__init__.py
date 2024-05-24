bcefb7a519ed9af837b6b725d1820154416297ed
mmgmolotov <mmgmolotov@gmail.com>
2024-05-24 02:38:35 +0100

    task 7 - 8 / fix

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" Initialize the views module """
+""" DocDocDocDocDocDoc
+"""
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
@@ -7,3 +8,5 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
 from api.v1.views.users import *
 from api.v1.views.session_auth import *
+
+User.load_from_file()
