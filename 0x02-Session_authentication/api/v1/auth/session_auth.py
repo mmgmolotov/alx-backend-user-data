f8b716e081a20d9eb2a163a845e0a8b0be41d654
mmgmolotov <mmgmolotov@gmail.com>
2024-05-24 01:12:52 +0100

    task 2 / fixed

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -23,5 +23,5 @@ class SessionAuth(Auth):
     def user_id_for_session_id(self, session_id: str = None) -> str:
         if session_id is None or not isinstance(session_id, str):
             return None
-         return self.user_id_by_session_id.get(session_id)
+        return self.user_id_by_session_id.get(session_id)
         
