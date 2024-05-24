1fe586c8d77d0ac14b20bb1ef1e9449174ad33ed
mmgmolotov <mmgmolotov@gmail.com>
2024-05-24 02:30:03 +0100

    task 7 - 8 / fix

--- 0x02-Session_authentication/models/user.py
+++ 0x02-Session_authentication/models/user.py
@@ -3,13 +3,15 @@
 """
 import hashlib
 from models.base import Base
-from models import storage
+
 
 class User(Base):
-    """ User class """
+    """ User class
+    """
 
     def __init__(self, *args: list, **kwargs: dict):
-        """ Initialize a User instance """
+        """ Initialize a User instance
+        """
         super().__init__(*args, **kwargs)
         self.email = kwargs.get('email')
         self._password = kwargs.get('_password')
@@ -18,19 +20,22 @@ class User(Base):
 
     @property
     def password(self) -> str:
-        """ Getter of the password """
+        """ Getter of the password
+        """
         return self._password
 
     @password.setter
     def password(self, pwd: str):
-        """ Setter of a new password: encrypt in SHA256 """
+        """ Setter of a new password: encrypt in SHA256
+        """
         if pwd is None or type(pwd) is not str:
             self._password = None
         else:
             self._password = hashlib.sha256(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd: str) -> bool:
-        """ Validate a password """
+        """ Validate a password
+        """
         if pwd is None or type(pwd) is not str:
             return False
         if self.password is None:
@@ -39,7 +44,8 @@ class User(Base):
         return hashlib.sha256(pwd_e).hexdigest().lower() == self.password
 
     def display_name(self) -> str:
-        """ Display User name based on email/first_name/last_name """
+        """ Display User name based on email/first_name/last_name
+        """
         if self.email is None and self.first_name is None \
                 and self.last_name is None:
             return ""
@@ -51,31 +57,3 @@ class User(Base):
             return "{}".format(self.last_name)
         else:
             return "{} {}".format(self.first_name, self.last_name)
-
-    @classmethod
-    def search(cls, attributes):
-        """ Search for users with matching attributes """
-        if attributes is None or type(attributes) is not dict:
-            return []
-        all_users = storage.all(User)
-        matching_users = []
-        for user in all_users.values():
-            match = True
-            for key, value in attributes.items():
-                if getattr(user, key) != value:
-                    match = False
-                    break
-            if match:
-                matching_users.append(user)
-        return matching_users
-
-    def to_json(self):
-        """ Returns a JSON representation of the User """
-        return {
-            "id": self.id,
-            "email": self.email,
-            "first_name": self.first_name,
-            "last_name": self.last_name,
-            "created_at": self.created_at,
-            "updated_at": self.updated_at
-        }
